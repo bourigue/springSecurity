41b77643f7227ed00775cd284dc59928af1c1899
bourigue <rrbourigue@gmail.com>
2022-07-23 12:40:43 +0100

    feat:add AccountServises

---
+++ src/test/java/com/example/springsecurity/Entites/userRole.java
@@ -0,0 +1,23 @@
+package com.example.springsecurity.Entites;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class userRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rolename;
+
+}
