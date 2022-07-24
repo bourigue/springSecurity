a4c959d6c1b9298ea8928536e051ce6ced61abeb
bourigue <rrbourigue@gmail.com>
2022-07-24 13:09:11 +0100

    feat:add class SecurityConfi

--- src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -1,7 +1,10 @@
 package com.example.springsecurity;
 
+import com.example.springsecurity.Services.AccountServicesImpl;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -10,4 +13,12 @@ public class SpringSecurityApplication {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(AccountServicesImpl accountServices,){
+    return args -> {
+
+
+    };
+
+    }
 }
--- src/test/java/com/example/springsecurity/SecurityConfi.java
+++
@@ -1,6 +0,0 @@
-package com.example.springsecurity;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-
-public class SecurityConfi extends WebSecurityConfiguration {
-}
