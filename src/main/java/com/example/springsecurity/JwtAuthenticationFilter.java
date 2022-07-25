ff35bf454c15b1a19504234531048c2eb4ac1e76
bourigue <rrbourigue@gmail.com>
2022-07-25 11:40:20 +0100

    fix: fixed the configurtaion of funcion  attempeAuthentication

--- src/main/java/com/example/springsecurity/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springsecurity/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.example.springsecurity;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -10,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.PasswordAuthentication;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
  private AuthenticationManager authenticationManager;
@@ -20,7 +22,16 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+        UsernamePasswordAuthenticationToken authenticationToken=
+                new UsernamePasswordAuthenticationToken(username,password);
+        return authenticationManager.authenticate(authenticationToken);
+
+
     }
 
     @Override
